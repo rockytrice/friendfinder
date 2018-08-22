33070e7f3d646f4b3264c2d25b1d403cbd7e6126
Roderick Trice <rocky_trice@yahoo.com>
2018-08-22 19:22:34 -0400

    added POST function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,24 @@
-
 var friendsData = require("../data/friends");
 
 
-module.exports = function(app) {
-// Displays all friends
-app.get("/api/friends", function(req, res) {
-    return res.json(friendsData);
-  });
+module.exports = function (app) {
+    // Displays all friends
+    // API GET Request
+    app.get("/api/friends", function (req, res) {
+        return res.json(friendsData);
+    });
 
 
-// app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
 
+        newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
+        console.log(newFriend);
+      
+        fiends.push(newFriend);
+      
+        res.json(newFriend);
 
+    })
 }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 
 
 
-// require("./routing/apiRoutes")(app);
-// require("./routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 
