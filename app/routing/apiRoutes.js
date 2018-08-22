52e074256559090aefd1f9ce0be8a273bcb6821b
Roderick Trice <rocky_trice@yahoo.com>
2018-08-22 17:50:32 -0400

    added export function for friendsdata

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,11 @@
 
+var friendsData = require("../data/friends");
+
+
 module.exports = function(app) {
 // Displays all friends
 app.get("/api/friends", function(req, res) {
-    return res.json(friends);
+    return res.json(friendsData);
   });
 
 
