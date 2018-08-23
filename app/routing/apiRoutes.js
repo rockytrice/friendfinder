5a3556a90a3266649b4616471764c9278a08a444
Roderick Trice <rocky_trice@yahoo.com>
2018-08-22 20:25:41 -0400

    adding on click function. still having trouble linking the server file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,11 +4,13 @@ var friendsData = require("../data/friends");
 module.exports = function (app) {
     // Displays all friends
     // API GET Request
+    // ===========================================================================
     app.get("/api/friends", function (req, res) {
         return res.json(friendsData);
     });
 
-
+  // API POST Requests
+// ================================================================================
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
 
@@ -16,7 +18,7 @@ module.exports = function (app) {
 
         console.log(newFriend);
       
-        fiends.push(newFriend);
+        friends.push(newFriend);
       
         res.json(newFriend);
 
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+// DEPENDENCIES
+// ===========================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
+// Sets up the Express App
+// ===========================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+// ============================================================
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 
-
 // Starts the server to begin listening
-// =============================================================
+// ============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
