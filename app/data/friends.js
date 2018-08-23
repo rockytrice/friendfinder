50b475f34f4972b6cbdd9ea7f916d1f3187ca60b
Roderick Trice <rocky_trice@yahoo.com>
2018-08-23 00:16:24 -0400

    added form validation and modal functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,39 +2,39 @@ var friends = [
  {
     "name": "sheldon copper",
     "photo": "https://giphy.com/gifs/uk3kYRDxfwQO4/html5",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 3, 5, 2, 5, 1, 4, 5, 4, 1]
  },
  {
     "name": "Raj Koothrappali",
     "photo": "https://media.giphy.com/media/uk3kYRDxfwQO4/giphy.gif",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 1, 2, 5, 1, 5, 3, 1, 2, 1]
  },
  {
     "name": "Penny",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 1, 5, 4, 5, 1, 3, 5, 4, 1]
  },
 
  {
     "name": "Howard Wolowitz",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 1, 3, 4, 5, 5, 2, 1, 4, 1]
  },
  {
      "name": "Bernadette",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 1, 5, 4, 5, 2, 2, 5, 3, 1]
  },
  
 {
     "name": "Amy Farrah Fowler",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 1, 6, 4, 1, 1, 2, 1, 4, 1]
  },
  {
      "name": "Leonard Hofstadter",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "scores": [5, 2, 4, 5, 5, 1, 2, 3, 4, 5]
  },
  {
     "name": "Stuart Bloom",
